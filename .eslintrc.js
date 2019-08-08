9eed4f0dab5a5c89c3ab3dea1c5148fa0033f7b0
zhangqingxue <1452428955@qq.com>
2019-08-08 10:20:51 +0800

    vue.js 初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-irregular-whitespace":"off",
+    "no-unused-vars":"off",
+    "no-console": "off", // 禁止console
+    "no-alert": "off", // 禁止alert,conirm等
+    "no-tabs": "off", // 禁止使用tab
+    "eol-last": "off", // 文件末尾强制换行
+    "no-new": "error",  // 禁止在使用new构造一个实例后不赋值
+    "no-trailing-spaces": "off", // 一行结束后面不要有空格
+    "indent": "off",
+    "key-spacing": "off",
+    "semi": "off", // 允许以分号结束
+    "space-before-function-paren": "off" // 函数定义时括号前面要不要有空格
+  }
+}
---
+++ src/components/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="login">
+    <div class="input-wrap-parent">
+      <div class="input-wrap">
+        <input type="text" v-model="name"/>
+        <span v-if="error.name" class="err-msg">{{error.name}}</span>
+      </div>
+      <div class="input-wrap">
+        <input type="password" v-model="pwd"/>
+        <span v-if="error.pwd" class="err-msg">{{error.pwd}}</span>
+      </div>
+      <div class="input-wrap">
+        <button @click="login">提交</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      pwd: '',
+      error: {
+        name: '',
+        pwd: ''
+      }
+    }
+  },
+
+  methods: {
+    check(name, pwd) {
+      if (!name) {
+        this.error.name = '请输入姓名'
+        return false
+      }
+      if (!pwd) {
+        this.error.pwd = '请输入密码'
+        return false
+      }
+      return true
+    },
+    login () {
+      const {name, pwd} = this;
+      if (!this.check(name, pwd)) return;
+      console.log('OK');
+      this.$router.push({name: 'main'});
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  /*.input-wrap-parent {*/
+    /*width: 300px;*/
+    /*margin: 10% auto;*/
+  /*}*/
+  .login{
+    background-size: cover;
+    background-image:url(../assets/yekong.jpg);
+    background-repeat:no-repeat;
+    background-position:center center;
+    height: 100%;
+    width: 100%;
+    /*background-attachment:fixed;*/
+    /*display: block;*/
+    position: fixed;
+  }
+</style>
