6a4d098cfc75fd7e3fb7b77cfc9d142a75e3843b
zhangqingxue <1452428955@qq.com>
2019-08-23 11:11:33 +0800

    去#号->失败!

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '../components/Home.vue'
+import Login from '../components/Login.vue'
 
 Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/Home',
+      name: 'Home',
+      component: Home
+    }
+  ]
+})
